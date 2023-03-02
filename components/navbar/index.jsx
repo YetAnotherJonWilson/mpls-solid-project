69d3bb1e337d1b7ea9f01a91a20ed1338295bd3a
Jon Wilson <jonwilsoncomedy@gmail.com>
2023-03-02 01:11:08 -0600

    building nav menu

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -6,12 +6,15 @@ export default function Navbar() {
     <div className={styles.navbar}>
       <div className={`${styles.nav1} bg-accent1-600`}>
         <img src="/images/icon-presentation-play.svg" width="50px"></img>
+        <p>Presentations</p>
       </div>
       <div className={`${styles.nav1} bg-accent2-300`}>
         <img src="/images/icon-user-group.svg" width="50px"></img>
+        <p>Members</p>
       </div>
       <div className={`${styles.nav1} bg-primary-500`}>
         <img src="/images/icon-application.svg" width="50px"></img>
+        <p>Apps</p>
       </div>
     </div>
   );
