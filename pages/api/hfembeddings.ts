80114e0ee3dc513d2f247e5c8172adcf8689a25f
Jon Wilson <jonwilsoncomedy@gmail.com>
2023-09-27 21:25:07 -0500

    removing accidentally merged files

--- pages/api/hfembeddings.ts
+++
@@ -1,8 +0,0 @@
-import { HfInference } from '@huggingface/inference'
-
-const hf = new HfInference(process.env.HF_TOKEN)
-
-await hf.featureExtraction({
-    model: "sentence-transformers/distilbert-base-nli-mean-tokens",
-    inputs: "That is a happy person",
-  });
